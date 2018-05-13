87e80824109f1141ca3f5892049bee99a9002402
renoud-grappin <loicrg69@gmail.com>
2018-05-13 05:42:32 +0200

    add

--- content/models.py
+++ content/models.py
@@ -1,6 +1,26 @@
 from django.db import models
 from django import forms
 from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
+from django.core.files.storage import FileSystemStorage
+from django.conf import settings
+from os.path import splitext, basename
+class PictureStorage(FileSystemStorage):
+    def __init__(self, *args, **kwargs):
+        kwargs.update({
+            'location': settings.MEDIA_ROOT,
+            'base_url': settings.MEDIA_URL
+        })
+        super(PictureStorage, self).__init__(*args, **kwargs)
+
+
+def upload_path(instance, filename):
+    filename = basename(filename)
+    _, extension = splitext(filename)
+    now = timezone.now()
+    prefix = 'pictures'
+    return '{}/{}/{}{}'.format(
+        prefix, now.year, '1', extension)
 
 class TypePost(models.Model):
 	typePost = models.CharField(max_length=200)
@@ -13,7 +33,7 @@ class Post(models.Model):
 	text = models.TextField(max_length=5000, blank=True,null=True)
 	serialNumberVideo = models.CharField(max_length=30, blank=True,null=True)
 	typePost = models.ManyToManyField(TypePost)
-	picturs = models.ImageField(blank=True,null=True)
+	picturs = models.ImageField(_('Image file'),upload_to=upload_path,storage=PictureStorage(),blank=True,null=True)
 	link = models.CharField(max_length=200, blank=True,null=True)
 	nameLink = models.CharField(max_length=200, blank=True,null=True)
 	publishPost = models.BooleanField(default=True)
--- content/urls.py
+++ content/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'contact', views.contact, name='contact'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

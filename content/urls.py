67a8b347fc3b21876f2c0157dddf21c1efe77635
renoud-grappin <loicrg69@gmail.com>
2018-05-13 06:53:51 +0200

    add login for admin

--- content/urls.py
+++ content/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'contact', views.contact, name='contact'),
     url(r'admin', views.admin, name='admin'),
+    url(r'login', views.login, name='login'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- content/views.py
+++ content/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.contrib.auth import authenticate, login as dj_login
+from pprint import pprint
 
 def post_list(request):
 	posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
@@ -14,3 +16,16 @@ def contact(request):
 def admin(request):
 	page = 'admin'
 	return render(request, 'content/admin.html', {"page":page})
+
+def login(request):
+	username = request.POST['username']
+	password = request.POST['password']
+	user = authenticate(request, username=username, password=password)
+	if user is not None:
+		dj_login(request, user)
+		page = 'admin ok'
+		return render(request, 'content/admin.html', {"page":page})
+	else:
+		page = 'admin nok'
+		return render(request, 'content/admin.html', {"page":page})
+

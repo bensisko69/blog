bed45102e0584a5fd1a6d91e431d587c9e8f5f83
renoud-grappin <loicrg69@gmail.com>
2018-05-13 02:51:16 +0200

    add static files and media files

--- blog/settings.py
+++ blog/settings.py
@@ -119,4 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    BASE_DIR + '/static/',
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR + '/media/'

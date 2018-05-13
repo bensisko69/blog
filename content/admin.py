d481378df5677c5ebed653bf1e89604ec5e6a59d
renoud-grappin <loicrg69@gmail.com>
2018-05-13 08:09:11 +0200

    add

--- content/admin.py
+++ content/admin.py
@@ -3,6 +3,8 @@ from .models import TypePost, Post
 
 class PostAdmin(admin.ModelAdmin):
 	list_display = ('title', 'text', 'created_date', 'publishPost')
+	list_filter = ('publishPost', 'created_date')
+	search_fields = ('created_date', 'title')
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(TypePost)

07af22f0db36acf8579b894a912822912f95850b
renoud-grappin <loicrg69@gmail.com>
2018-05-13 04:34:25 +0200

    add page contact

--- content/views.py
+++ content/views.py
@@ -5,3 +5,6 @@ from .models import Post
 def post_list(request):
 	posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
 	return render(request, 'content/post.html', {'posts': posts})
+
+def contact(request):
+	return render(request, 'content/contact.html')
